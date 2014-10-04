cb4895999e4019a4c55703b7c56d5a6c928451ee
tomykaira <tomykaira@gmail.com>
2014-10-05 02:27:58 +0900

    Force captial & alphanumeric at name input

--- webpay-token/src/main/java/jp/webpay/android/ui/field/NameField.java
+++ webpay-token/src/main/java/jp/webpay/android/ui/field/NameField.java
@@ -1,13 +1,17 @@
 package jp.webpay.android.ui.field;
 
 import android.content.Context;
+import android.text.Editable;
 import android.text.InputType;
+import android.text.TextWatcher;
 import android.util.AttributeSet;
 import jp.webpay.android.R;
 import jp.webpay.android.model.RawCard;
 import jp.webpay.android.validator.NameValidator;
 
-public class NameField extends BaseCardField {
+import java.util.Locale;
+
+public class NameField extends BaseCardField implements TextWatcher {
     private String mValidName;
 
     public NameField(Context context) {
@@ -28,7 +32,9 @@ public class NameField extends BaseCardField {
     private void initialize() {
         setHint(R.string.field_name_hint);
         setInputType(InputType.TYPE_CLASS_TEXT |
-                InputType.TYPE_TEXT_VARIATION_PERSON_NAME);
+                InputType.TYPE_TEXT_FLAG_CAP_CHARACTERS |
+                InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS);
+        addTextChangedListener(this);
     }
 
     @Override
@@ -46,6 +52,29 @@ public class NameField extends BaseCardField {
     @Override
     public void updateCard(RawCard card) {
         card.name(mValidName);
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+        String string = s.toString();
+        changeText(string.replaceAll("[^A-Za-z ]", "").toUpperCase(Locale.ENGLISH));
+    }
+
+    private void changeText(String s) {
+        removeTextChangedListener(this);
+        int nextSelection = getSelectionEnd() - getText().length() + s.length();
+        setText(s);
+        setSelection(nextSelection);
+        addTextChangedListener(this);
+    }
+
+    @Override
+    public void onTextChanged(CharSequence text, int start, int lengthBefore, int lengthAfter) {
+        super.onTextChanged(text, start, lengthBefore, lengthAfter);
     }
 
     /**
