0cc6791a13563da29707ccb9f643fdfd9415aacd
hmsk <k.hamasaki@gmail.com>
2015-01-10 21:32:13 +0900

    Adjust style, layout

--- sample/src/main/java/jp/webpay/android/sample/BaseFragmentActivity.java
+++ sample/src/main/java/jp/webpay/android/sample/BaseFragmentActivity.java
@@ -22,7 +22,7 @@ public abstract class BaseFragmentActivity extends ActionBarActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_info) {
             return true;
         }
         return super.onOptionsItemSelected(item);
