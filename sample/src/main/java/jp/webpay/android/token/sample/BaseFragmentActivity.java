88b5708a59d89e91f94b04347534f1eaa8ede0ad
hmsk <k.hamasaki@gmail.com>
2015-01-14 00:47:36 +0900

    Add back interface each activities except SampleListActivity

--- sample/src/main/java/jp/webpay/android/token/sample/BaseFragmentActivity.java
+++ sample/src/main/java/jp/webpay/android/token/sample/BaseFragmentActivity.java
@@ -1,5 +1,6 @@
 package jp.webpay.android.token.sample;
 
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -13,6 +14,7 @@ public abstract class BaseFragmentActivity extends ActionBarActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.webpay_token_sample, menu);
         super.onCreateOptionsMenu(menu);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(enableBackOnActionBar());
         return true;
     }
 
@@ -21,10 +23,18 @@ public abstract class BaseFragmentActivity extends ActionBarActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_info) {
-            return true;
+        switch (item.getItemId()) {
+            case R.id.action_info:
+                return true;
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
-        return super.onOptionsItemSelected(item);
+    }
+
+    protected boolean enableBackOnActionBar() {
+        return true;
     }
 }
